5e086f7701705d77ba476cbb7be64e2145a7eb1c
thefont <eric.m.font@gmail.com>
2013-08-28 16:45:37 -0500

    Fixed some code formatting issues

--- modules/Admin/showSection.php
+++ modules/Admin/showSection.php
@@ -2,10 +2,9 @@
 
 // The section we're supposed to look up
 ($targetSection = @$URI[0]) || ($targetSection = @$URI["section"]);
-
+// Wrapper for the admin section
 echo "
 	<span class='Admin-section'>";
-
 // If section is Null, then call overview
 if(!$targetSection) Jackal::call("Admin/overview");
 
@@ -16,13 +15,14 @@ else {
 	$action = Jackal::siteURL("Admin/save/$targetSection");
     // Run the test for this section and get the test's errors / warnings
     $results = $this->runTests(array($targetSection));
-
+    // Create an interface for all the settings and messages
     echo "
 		<form \$='.Admin-section' method='post' action='$action'>
 		<h1>$targetSection</h1>";
 
 	// Go through the subsections of the target section
 	foreach((array) @$sections[$targetSection] as $subsectionName=>$subsection) {
+        // Show the subsection's title and start the region for messages
 	    echo "
 			<h2>$subsectionName</h2>
             <span class='message-area'>
@@ -39,20 +39,23 @@ else {
             }
         }
 
+        // End the message region
         echo "
                 </ul>
             </span>";
 		
 		// Go through all the editors that are supposed to show up in this subsection
-	    foreach((array) $subsection as $editor) {
+	    foreach((array) $subsection as $editor)
+            // Run the callback to load the settings for this subsection
 	        Jackal::call($editor["callback"]);
-	    }
-	}	
-	
+	}
+
+    // Show the save button and close the form
     echo "
 			<button type='submit'>Save</button>
 		</form>";
 }
 
+// End the admin-section area
 echo "
 	</span>";
