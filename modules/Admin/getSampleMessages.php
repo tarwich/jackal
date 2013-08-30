7406e1a94b16f270c0d0293afec708dfe6abe609
Eric Fontenault <eric.m.font@gmail.com>
2013-08-30 17:17:41 -0500

    Wrote the test for overviewAlert

---
+++ modules/Admin/getSampleMessages.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This method returns an array of test messages.
+ *
+ * This method will return an array containing one message of each priority level. This is used
+ * for Testing purposes
+ *
+ * @return array
+ */
+
+// Construct and return an array with test messages in all categories for testing purposes
+return array(
+    "ERROR"   => array("error test"),
+    "WARNING" => array("warning test"),
+    "INFO"    => array("info test"),
+);
