cc80298ad219ba04b69aa2360688e50a62ac9dcb
Eric Fontenault <eric.m.font@gmail.com>
2013-09-03 09:55:28 -0500

    Fixes for i69. Removed some unnecessary code, fixed the title of the sub-test, used assertEquals to evaluate if the test was successful (as opposed to assertTrue), and fixed several very wrong comments,

--- modules/Admin/tests/overviewAlert.php
+++ modules/Admin/tests/overviewAlert.php
@@ -3,38 +3,33 @@
 /**
  * This method tests if showOverviewAlerts is working correctly
  *
- * This method checks the number of alerts returned from showOverViewAlerts by creating a test
+ * This method checks response of showOverViewAlerts by creating a test
  * module with a self-test of "getSampleMessages," which returns an array with one message of
  * each priority level. Since showOverviewAlerts only considers "errors" and "warnings," it expects
- * to receive an alert of 2.
+ * to receive an html element with text equaling 2.
  *
  * @return void
  */
 
-$this->startSubTest(array("testWritten"));
-
-// Backup the admin settings
-$oldSettings = Jackal::setting("admin");
-// Make a copy of the settings to change
-$settings = $oldSettings;
-// Set the timezone to something invalid
+// Set the title of the sub-test
+$this->startSubTest(array("Verify correct response from Admin/runTests/getSampleMessages"));
+// Set the timezone to something invalid, and create a test module that will run the getSampleMessages test
 Jackal::putSettings("
 admin:
     modules:
             testModule:
                 -
                     name     : Foo / Bar
-                    callback : Bin / Baz
                     self-test: Admin/getSampleMessages
 jackal:
     timezone: Foo/Bar
 ");
-// Run the localization test. At least 1 test should fail (the timezone)
+// Get the overview alerts for the Foo module. It should return an html element with text
+// containing the number of errors/warnings.
 $results = Jackal::returnCall("Admin/showOverviewAlerts/Foo");
-// Strip the html tags
+// Strip the html tags to isolate the number of results
 $results = strip_tags($results);
-// Create our expected result -- This method only counts errors and warnings, so given one of each
-// message type, our expected result is 2.
-$expected = "2";
+// Our expected result is 2, since we are only considering errors and warnings.
+$expected = 2;
 // Compare what we got vs what we expected
-$this->assertTrue(array(($results === $expected) ? true : false));
+$this->assertEquals(array($expected, $results));
