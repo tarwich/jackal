051c436a99e11a493b639baeeef5cf133d5e18db
Eric Fontenault <eric.m.font@gmail.com>
2013-09-03 16:07:58 -0500

    Completed writing the test for runTests

--- modules/Admin/tests/runTests.php
+++ modules/Admin/tests/runTests.php
@@ -1,29 +1,70 @@
 <?php
 
-$this->startSubTest("All Foo");
-// Backup the admin settings
-$oldSettings = Jackal::setting("admin");
-// Make a copy of the settings to change
-$settings = $oldSettings;
-// Set the modules
+/**
+ * This test checks to see if runTests is working correctly
+ *
+ * This test works by creating a testModule with a self-test that runs getSampleMessages.
+ * It then compares the results of that self-test with the array of messages that
+ * we expected to receive. If they match, then the test was successful. Otherwise,
+ * the test was unsuccessful
+ *
+ * @return void
+ */
+
+// Create a test module with multiple subsections
 Jackal::putSettings("
 admin:
 	modules:
-		testModule:
-			- 
+		subsection1:
+			-
 				name     : Foo / Bar
-				callback : bin/Baz
-				self-test: Testing/returnValue/11.22
+				self-test: Admin/getSampleMessages
+		subsection2:
+			-
+				name     : Foo / Bin
+				self-test: Admin/getSampleMessages
 ");
+
+//  __________________________________________________
+// / Test single   sub-section                        \
+
+// Set the title for this test
+$this->startSubTest(array("Run sample test on a specific sub-module"));
+// Run just the Bar submodule
+$results = Jackal::call("Admin/runTests/Foo/Bar");
+// Create our array of expected messages
+$expected = array(
+    "ERROR"   => array("error test"),
+    "WARNING" => array("warning test"),
+    "INFO"    => array("info test"),
+);
+// Compare our results with our expected results
+$this->assertEquals(array($expected, $results));
+
+// \__________________________________________________/
+
+//  __________________________________________________
+// / Test multiple sub-sections                       \
+
+// Set the title for the sub-test
+$this->startSubTest(array("Run sample test all of a module's sub-modules"));
 // Run all tests in the foo section
 $results = Jackal::call("Admin/runTests/Foo");
-
+// Create our array of expected messages. Both sub-modules call getSampleMessages, so we need 2 of each error message
 $expected = array(
-	"ERROR"   => array(),
-	"WARNING" => array(),
-	"INFO"    => array("11.22"),	
+    "ERROR"   => array(
+        "error test",
+        "error test"
+    ),
+    "WARNING" => array(
+        "warning test",
+        "warning test"
+    ),
+    "INFO"    => array(
+        "info test",
+        "info test"
+    ),
 );
-
-$actual = $results;
-
-$this->assertEquals(array($expected, $actual));
+// Compare our results with our expected results
+$this->assertEquals(array($expected, $results));
+// \__________________________________________________/
