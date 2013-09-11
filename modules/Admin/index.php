13806cb8fddbd807fc497e4a86a5168a7c389e3d
Samuel Dillow <tarwich@yahoo.com>
2013-09-11 12:13:30 -0500

    Added Console to admin

--- modules/Admin/index.php
+++ modules/Admin/index.php
@@ -14,10 +14,12 @@ js("resources/jquery-1.10.2.min.js");
 js("admin.js");
 // Ensure url.js is available
 js("url.js");
-//Load Google Font - This is likely temporary, in the event that Charles gets his way and we include his remote fonts.
+// Load Google Font - This is likely temporary, in the event that Charles gets his way and we include his remote fonts.
 css("http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800");
-//Load Default Stylesheet
+// Load Default Stylesheet
 css("styles.css");
+// Show the console
+Jackal::call("Console/show");
 
 ?><? 
 	// This is the Header, Obviously 
